7301b99f11159eb80ef4e0c4ab8de32eb9627023
Don Nana <nana15@validusprep.org>
2013-07-16 15:45:51 -0400

    Blogger

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = 'edddeb89f90e770c1c9d8f0b26a920ba0560d86d476059a16649dc769ccb83c1b7d51d3df5c3e2c00a7a31f1c1e6bc22aa7861ce5407451f0e53813e46d0c2c0'
